6606a8b76c6d4583d277b58b88e102efc000c109
Lester Padul <padullester@gmail.com>
2015-05-29 01:35:05 +0000

    testing

--- index.php
+++ index.php
@@ -65,13 +65,13 @@
  */
 switch (ENVIRONMENT)
 {
-	case 'development':
+	case 'development asdasdasdasd':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
 
-	case 'testing':
-	case 'production changing this line':
+	case 'testing' asd asd asd:
+	case 'production changing this lineasdasdasd aasd asd':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
