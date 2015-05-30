691e9a7005688591068427f5f24af4131a657a81
unknown <johnrobox>
2015-05-30 16:57:29 -0700

    added on welcome message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -77,6 +77,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <p>
                     A rapid development framework
                 </p>
+                <p>
+                    We love CodeIgniter
+                </p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
