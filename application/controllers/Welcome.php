2ab297a20c967de22e39fab7bb3c3e904f9e5488
unknown <johnrobox>
2015-05-30 03:42:28 -0700

    creating hello function

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
-		echo 'hello';
-		echo 'hello world';
+	}
+	public function hello(){
 	}
 }
