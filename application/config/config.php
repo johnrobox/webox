5a8ef50fbb7e3ec2df16d6fd18280389442a3b0f
john robert jerodiaz <johnrobertjerodiaz@gmail.com>
2015-06-20 08:50:33 -0700

    configuration complete

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/webox/';
 
 /*
 |--------------------------------------------------------------------------
@@ -305,7 +305,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '0rGBISBT61k7453Ooq6343KjtGe9Xjv4';
 
 /*
 |--------------------------------------------------------------------------
