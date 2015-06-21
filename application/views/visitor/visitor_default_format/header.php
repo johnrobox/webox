42b285a9c4b589a3871e6935055f05563e759c97
john robert jerodiaz <johnrobertjerodiaz@gmail.com>
2015-06-20 19:29:24 -0700

    homepage

---
+++ application/views/visitor/visitor_default_format/header.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="application-name" content="Webox : Easy job post and hunting"/>
+        <meta name="google-site-verification" content="FfrIYMK3Mo3WbWe05Wi4hSkFZ2WncJyDTma0x42JljE" />
+        <title>Webox : Easy job post and hunting</title>
+        <meta name="description" content="Easy job searh and posting in Bacolod, Cagayan de Oro, Cebu, Davao, Iloilo. We are now lunch our site with free jobposting for the employeer and easily search for the people who very in need job.">
+        <link href="/user/favicon.ico?v=1" type="image/x-icon" rel="icon" />
+        <link href="/user/favicon.ico?v=1" type="image/x-icon" rel="shortcut icon" />
+        
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+
+        
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap-theme.min.css">
+        
+        <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+        
+    </head>
+    <body style="background: rgba(245, 245, 245, 1);">
+        <div class="container-fluid">
+            <div class="breadcrumb">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1 style="font-size: 75px; font-weight: bold; color:yellowgreen;">JOBS HERE!</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="<?php echo base_url();?>img/header_logo.png"/>
+                    </div>
+                </div>
+            </div>
+            <div class="breadcrumb">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="alert alert-success">
+                            <a href="" class="btn btn-info btn-block" style="font-size:20px;">Bacolod</a>
+                            <a href="" class="btn btn-info btn-block" style="font-size:20px;">Cagayan de Oro</a>
+                            <a href="" class="btn btn-info btn-block" style="font-size:20px;">Cebu</a>
+                            <a href="" class="btn btn-info btn-block" style="font-size:20px;">Davao</a>
+                            <a href="" class="btn btn-info btn-block" style="font-size:20px;">Iloilo</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="<?php echo base_url();?>img/left_content.png"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
