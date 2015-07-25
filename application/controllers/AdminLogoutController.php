9e00e6bcbf3b18c8c09b980d302f1f47cddd375b
John Robert Jerodiaz <johnrobertjerodiaz@gmail.com>
2015-07-25 03:26:51 -0700

    Admin Logout

---
+++ application/controllers/AdminLogoutController.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Author :  Webox Team
+ * Date created : June 27, 2015.
+ * Place : Boarding House
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminLogoutController extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->adminSes = array(
+            'AdminId',
+            'AdminFirstname',
+            'AdminLastname',
+            'AdminUsername',
+            'AdminApi'
+        );
+    }
+    
+    public function index(){
+        $this->session->unset_userdata($this->adminSes);
+        $this->session->sess_destroy();
+        redirect(base_url().'index.php/AdminLoginController');
+        exit();
+    }
+}
--- application/views/admin/admin_default_format/admin-top-menu.php
+++ application/views/admin/admin_default_format/admin-top-menu.php
@@ -13,11 +13,9 @@
                   <span class="caret"></span>
               </a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
+                <li><a href="#">Settings</a></li>
                 <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
+                <li><a href="<?php echo base_url(); ?>index.php/AdminLogoutController">Logout</a></li>
               </ul>
             </li>
           </ul>
