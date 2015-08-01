a24a5309647173c43d9a5ee0d368111ee078284e
John Robert Jerodiaz <johnrobertjerodiaz@gmail.com>
2015-08-01 04:12:52 -0700

    employer about posting job / post job

--- application/views/visitor/employer_modal/employer-about-posting-job.php
+++ application/views/visitor/employer_modal/employer-about-posting-job.php
@@ -6,10 +6,26 @@
                 About Posting Job
             </div>
             <div class="modal-body">
+                <h3>
+                    PHP 1,680.00 for 30-days per position*
+                </h3>
+            
+                * 7-days per Call Center position(s). 
+                <br>
+                The following qualify for free posting:
+                <br>
+                
+                <ul>
+                    <li>Government jobs</li>
+                    <li>Non-profit organization jobs (with official documents)</li>
+                    <li>Skilled work & blue collar jobs</li>
+                    <li>Nautical and Agricultural jobs</li>
+                    <li>Internship jobs for students</li>
+                </ul>
                 
             </div>
             <div class="modal-footer">
-                <button class="btn btn-primary btn-xs" title="Close" data-dismiss="modal" >Ok</button>
+                <button class="btn btn-primary" title="Close" data-dismiss="modal" >Ok</button>
             </div>
         </div>
     </div>
--- application/views/visitor/employer_pages/post-job.php
+++ application/views/visitor/employer_pages/post-job.php
@@ -2,7 +2,67 @@
 <div class="jumbotron">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6 col-sm-offset-3">
+            
+            
+            <div class="col-sm-5 col-sm-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="fa fa-pencil"></span>
+                        Company Information
+                    </div>
+                    <div class="panel-body">
+                        <?php echo form_open(); ?>
+                        <label for="companyProfile">Browse company profile</label>
+                        <input type="file" name="companyProfile" class="form-control" />
+                        
+                        <div class="form-group">
+                            <label for="companyName">Company Name</label>
+                            <input type="text" name="companyName" id="companyName" class="form-control" placeholder="Enter company name"/>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="companyAddressOne">Address One</label>
+                            <small> ( Street Address )</small>
+                            <input type="text" name="companyAddressOne" id="companyAddressOne" class="form-control" placeholder="Enter Address One."/>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="companyAddressTwo">Address Two</label>
+                            <small> ( Barangay / Landmark ) </small>
+                            <input type="text" name="companyAddressTwo" id="companyAddressTwo" class="form-control" placeholder="Enter Address  Two"/>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="companyCity">City</label>
+                            <input type="text" name="companyCity" id="companyCity" class="form-control" placeholder="City address" />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="companyProvince">Province / State</label>
+                            <input type="text" name="companyProvince" id="companyProvince" class="form-control" placeholder="Province address" />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="companyCountry">Country</label>
+                            <input type="text" name="companyCountry" id="companyCountry" class="form-control" placeholder="Country"/>
+                        </div>
+                        
+                        <label for="companyDescription">Company Description </label>
+                        <textarea id="companyDescription" rows="10" class="form-control">
+                        
+                        </textarea>
+                        <input type="submit" class="btn btn-primary" value="Add"/>
+                        <input type="reset" class="btn btn-default" value="Reset"/>
+                        <?php echo form_close(); ?>
+                    </div>
+                    <div class="panel-footer">
+                        
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-5">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <span class="fa fa-pencil"></span>
@@ -11,54 +71,57 @@
                     <?php echo form_open(); ?>
                     <div class="panel-body">
                         
-                        <label for="position">Position Name</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-addon">
-                                <span class="fa fa-user"></span>
-                            </div>
-                            <input type="text" name="position" id="position" class="form-control"/>
+                        <div class="form-group">
+                            <label for="jobTitle">Job Title</label>
+                            <input type="text" name="jobTitle" id="jobTitle" class="form-control" placeholder="your job title"/>
                         </div>
                         
-                        <label for="jobLevel">Job Level </label>
-                        <select name="jobLevel" id="jobLevel" class="form-control">
-                            <option value="ojt">OJT</option>
-                            <option value="partime">Partime</option>
-                            <option value="entry">Entry Level</option>
-                            <option value="0-1">0 - 1 year</option>
-                            <option value="1-2">1 - 2 years</option>
-                            <option value="2-3">2 - 3 years</option>
-                            <option value="3-4">3 - 4 years</option>
-                            <option value="4-5">4 - 5 years</option>
-                            <option value="5-up">More than 5 years</option>
-                        </select>
-                        
-                        <label for="company">Company</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-addon">
-                                <span class="fa fa-user"></span>
-                            </div>
-                            <input type="text" name="company" id="company" class="form-control"/>
+                        <div class="form-group">
+                            <label for="jobLevel">Job Level </label>
+                            <select name="jobLevel" id="jobLevel" class="form-control">
+                                <option value="ojt">OJT</option>
+                                <option value="partime">Partime</option>
+                                <option value="entry">Entry Level</option>
+                                <option value="0-1">0 - 1 year</option>
+                                <option value="1-2">1 - 2 years</option>
+                                <option value="2-3">2 - 3 years</option>
+                                <option value="3-4">3 - 4 years</option>
+                                <option value="4-5">4 - 5 years</option>
+                                <option value="5-up">More than 5 years</option>
+                            </select>
                         </div>
                         
-                        <label for="addressOne">Company Address </label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-addon">
-                                <span class="fa fa-user"></span>
-                            </div>
-                            <input type="text" name="addressOne" id="addressOne" class="form-control"/>
+                        <div class="form-group">
+                            <label for="jobDescription">Job Description</label>
+                            <textarea id="jobDescription" name="jobDescription" rows="8" class="form-control">
+
+                            </textarea>
                         </div>
                         
-                        <label for="companyDescription">Company Description</label>
-                        <textarea name="companyDescription" id="companyDescription" class="form-control"></textarea>
+                        <div class="form-group">
+                            <label for="jobQualification">Job Qualification</label>
+                            <textarea id="jobQualification" name="jobQualification" rows="8" class="form-control">
+
+                            </textarea>
+                        </div>
                         
-                        <label for="jobDescription">Job Description</label>
-                        <textarea name="jobDescription" id="jobDescription" class="form-control"></textarea>
+                        <div class="form-group">
+                            <label for="numberVacancies">Number of Vacancies</label>
+                            <select name="numberVacancies" id="numberVacancies" class="form-control">
+                                <option value="">Select number of vacancies</option>
+                                <?php for($i = 1 ;$i<50; $i++) { ?>
+                                <option  value="<?php echo $i; ?>"> <?php echo $i; ?> </option>
+                                <?php } ?>
+                                <option value="0">More</option>
+                            </select>
+                        </div>
                         
-                        <label for="jobQualification">Job Qualification</label>
-                        <textarea name="jobQualification" for="jobQualification" class="form-control"></textarea>
-                        
-                        <label for="video">Embed Video</label>
-                        <textarea name="video" for="video" class="form-control"></textarea>
+                        <div class="form-group">
+                            <label for="howToApply">How to Apply</label>
+                            <textarea id="howToApply" name="howToApply" rows="8" class="form-control">
+
+                            </textarea>
+                        </div>
                         
                     </div>
                     <div class="panel-footer">
